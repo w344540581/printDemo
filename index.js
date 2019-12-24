fed248b58bc2287fb9af122bb141fa9bb9950fe1
kangKANG0225 <344540581@qq.com>
2019-12-24 17:04:58 +0800

    'upadte'

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * @format
- */
+import {NativeModules} from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import Demo from './Demo'
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Demo);
+export default NativeModules.PrintFilesName;
